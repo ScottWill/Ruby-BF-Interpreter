81e22ce79d8fb3eada424fbcc5e1921c885928e3
Scott Will <scottdwill@gmail.com>
2011-01-23 19:16:53 -0600

    fixed loop bug

--- brainfuck.rb
+++ brainfuck.rb
@@ -1,6 +1,7 @@
 class Brainfuck
   
-  attr_accessor :raw_code #allow access to the raw code (good for just loading the code file to strip out everything)
+  attr_accessor :raw_code 
+  attr_accessor :opt_code
   
   def initialize(bf_file)
     @raw_code = '' #holds the raw bf code
@@ -30,8 +31,9 @@ class Brainfuck
     #at the pointer, so shortcut it
     @raw_code = @raw_code.gsub(/\[-\]/, '=')
     
-    #probably a more ruby-esque way to do this... find all optimizable commands
-    #and find how many times they appear in order (sequential commands)
+    #tranform the bf script into an optimized array, ie
+    # "++++++++++[>+++++++>++++++++++>+++>+<<<<-][-]" becomes
+    # [ '+', 10, '[', X, '>', 1, '+', 7, '>', 1, '+', 10, '>', 1, '+', 3, '>', 1, '+',1, '<', 4, '-', 1,']', X, '=', 0 ]
     i = 0
     while i < @raw_code.length do
       c, t = @raw_code[i, 1], @raw_code[i + 1, 1]
@@ -60,71 +62,111 @@ class Brainfuck
     #jump to that point instead of constantly having to look it up during run-time
     while i < @opt_code.length do
       l = i
-      case @opt_code[i]
-        when '['
-          stop, j = false, 0
-          while !(stop) do
-            i += 2
-            case @opt_code[i]
-              when ']'
-                stop = (j == 0)
-                j -= 1
-              when '['
-                j + 1
-            end
+      if @opt_code[i] == '['
+        stop, j = false, 0
+        until stop do
+          i += 2
+          case @opt_code[i]
+            when ']'
+              stop = (j == 0)
+              j -= 1
+            when '['
+              j += 1
           end
-          
-          @opt_code[l + 1], @opt_code[i + 1] = i, l
+        end
+        
+        @opt_code[l + 1], @opt_code[i + 1] = i, l
       end
       
-      i += 1
+      i = l
+      i += 2
     end
     
     #puts @opt_code
   end
   
+  
   #run the bf code
   def interperate
+  
+    cap = 1000000 #maximum number of 'operations' allowed
+    ops = 0
+  
     mem = Array.new(30000, 0)
+    #mem.pack("c*")
     ptr, v = 0, 0
     
-    i = 0
-    while i < @opt_code.length do
-      case @opt_code[i]
-        when '+' #increment that value @ pointer
-          mem[ptr] += @opt_code[i + 1]
-        when '-' #decrement the value @ pointer
-          mem[ptr] -= @opt_code[i + 1]
-        when '=' #nil (zero-out) the value @ pointer
-          mem[ptr] = 0
-        when ',' #read in
-          #mem[ptr] = get_character.bytes
-        when '.' #write out value (as char) at the pointer
-          print mem[ptr].chr
-        when '>' #increment the pointer
-          ptr += @opt_code[i + 1]
-        when '<' #decrement the pointer
-          ptr -= @opt_code[i + 1]
-        when '[' #begin loop
-          i = (mem[ptr] == 0) ? @opt_code[i + 1] : i
-        when ']' #end loop
-          i = (mem[ptr] != 0) ? @opt_code[i + 1] : i
+    #begin
+      i = 0
+      while i < @opt_code.length && ops < cap do
+        case @opt_code[i]
+          when '+' #increment that value @ pointer
+            mem[ptr] += @opt_code[i + 1]
+          when '-' #decrement the value @ pointer
+            mem[ptr] -= @opt_code[i + 1]
+          when '=' #nil (zero-out) the value @ pointer
+            mem[ptr] = 0
+          when ',' #read in
+            #mem[ptr] = get_character.bytes
+          when '.' #write out value (as char) at the pointer
+            print mem[ptr].chr
+          when '>' #increment the pointer
+            ptr += @opt_code[i + 1]
+          when '<' #decrement the pointer
+            ptr -= @opt_code[i + 1]
+          when '[' #begin loop
+            i = (mem[ptr] == 0) ? @opt_code[i + 1] : i
+          when ']' #end loop
+            i = (mem[ptr] != 0) ? @opt_code[i + 1] : i
+        end
+        
+        #while mem[ptr] > 255
+        #  mem[ptr] -= 256
+        #end
+        
+        #ops += 1
+        i += 2
       end
-      
-      i += 2
-    end
-    
+    #rescue
+    #  puts ""
+    #end
   end
   
   private :optimize
 end
 
-if __FILE__ == $0 #only run if this is the main file
+if __FILE__ == $0
   bf_file = ARGV[0]
   if bf_file.nil? || bf_file.empty? || bf_file == '-?'
-      abort("Usage: ruby brainf*ck.rb hello_world.b")
+      abort("Usage: ruby brainfuck.rb hello_world.b [-v]")
   end
   
   bf = Brainfuck.new(bf_file)
+  
+  if ARGV[1] == '-v'
+    puts "Raw:\n#{bf.raw_code}\n\nOptimized:\n#{bf.opt_code.to_s}\n\n--BEGIN--"
+  end
+  
   bf.interperate
+  
+  if ARGV[1] == '-v'
+    puts '--END--'
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
